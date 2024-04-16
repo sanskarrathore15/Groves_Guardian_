5d67c626a6943ad56814d307d4f5060a34cce312
Sanskar Rathore <sanskarrathore1553+github@gmail.com>
2024-04-17 01:30:21 +0530

    minors

--- Assets/animation.cs
+++ Assets/animation.cs
@@ -80,4 +80,6 @@ public class EnemyController : MonoBehaviour
         // Resume walking
         animator.SetBool("canWalk", true);
     }
+
+   
 }
---
+++ Assets/destroy_enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.Timeline;
+
+public class destroy_enemy : MonoBehaviour
+{
+    public int collisionCount = 3; // Number of collisions required to destroy the GameObject
+    private int currentCollisions = 0; // Counter for the collisions
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Attack"))
+        {
+
+            currentCollisions++;
+
+            Destroy(other.gameObject);
+            Debug.Log("destroy_other");
+
+            if (currentCollisions >= collisionCount)
+            {
+                Destroy(gameObject);
+                Debug.Log("Destroy_itself");
+            }
+            else
+            {
+                Debug.Log("Collision with Enemy. " + (collisionCount - currentCollisions) + " more collision(s) required.");
+            }
+        }
+    }
+
+
+    //public void OnCollisionEnter(Collision other)
+    //{
+    //    if (other.gameObject.CompareTag("Enemy"))
+    //    {
+    //        // Increment the collision count
+    //       // currentCollisions++;
+
+    //        // Destroy the other GameObject
+    //        Destroy(other.gameObject);
+
+    //        // Check if the required number of collisions has occurred
+    //    //    if (currentCollisions >= collisionCount)
+    //    //    {
+    //    //        // Destroy this GameObject
+    //    //        Destroy(other.gameObject);
+    //    //    }
+    //    //    else
+    //    //    {
+    //    //        // Play some effect or sound indicating a collision
+    //    //        Debug.Log("Collision with Enemy. " + (collisionCount - currentCollisions) + " more collision(s) required.");
+    //    //    }
+    //    }
+    //}
+}
